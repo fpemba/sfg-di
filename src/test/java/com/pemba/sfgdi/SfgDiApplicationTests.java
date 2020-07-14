1f9c1fa3cf6d39c4f18d00afd283cb80364de70e
fpemba <fatuma.pemba@yahoo.com>
2020-07-13 20:57:27 -0400

    Initial project add

---
+++ src/test/java/com/pemba/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pemba.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
